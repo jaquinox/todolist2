9e48fc7182efdb4817d5d591045774de25731cfc
aquino <jaquinox123@gmail.com>
2025-01-11 16:37:59 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import AppHeader from './components/AppHeader.vue';
+import ListItem from './components/ListItem.vue';
+
+</script>
+
+<template>
+
+  <AppHeader />
+
+  <ListItem />
+
+
+</template>
+
+<style scoped>
+</style>
